0dfb5a192d931c037b90ae025b14bd79dd90c6bc
TAPIA Frédéric <frederic@eduroam-n2ng-149204.local.univ-savoie.fr>
2017-09-25 15:52:44 +0200

    Modification

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <? php if(have_posts()):while(have_posts()):
   the_post(); ?>
 
-<section> <h3> <?php the_title(); ?> Titre swag </h3> </section>
+<section> <h3> <?php the_title(); ?> Titre du swag </h3> </section>
 <p> <?php the_content(); ?> </p>
 
   <?php   get.footer();
