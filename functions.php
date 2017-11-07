7d5af04785e3a99c73739e50ef75f1b7ffc0a0ef
TAPIA Frédéric <frederic@vpn-243229.local.univ-savoie.fr>
2017-11-07 09:32:41 +0100

    Ajout css function

--- functions.php
+++ functions.php
@@ -8,4 +8,11 @@
 
   add_action('after_setup_theme','theme_support');
 
+  /*Ajout CCS et JS*/
+  function scripts(){
+    wp_enqueue_style('style',get_stylesheet_uri());
+    wp_enqueue_style('style',get_template_directory_uri().'style.css');
+  }
+
+  add_action('up_enqueue_script','scripts');
 ?>
