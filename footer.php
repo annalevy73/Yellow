87f1ee346e9b71b34e67e3c2dbf979a350e51d38
TAPIA Frédéric <frederic@vpn-243234.local.univ-savoie.fr>
2017-11-07 10:51:44 +0100

    a

--- footer.php
+++ footer.php
@@ -1,7 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="style.css"/>
 
-<footer>
+    <?php bloginfo('name');?>
+    <?php bloginfo('description');?>
+    <?php wp_head(); ?>
+  </head>
 
-  <p>Copyright TAPIA Frédéric et LÉVY Anna</p>
-  <?php wp_footer(); ?>
+  <body>
 
-</footer>
+    <footer>
+      <p>Copyright TAPIA Frédéric et LÉVY Anna</p>
+      <?php wp_footer(); ?>
+    </footer>
+
+  </body>
+</html>
--- page-template/actualités.php
+++ page-template/actualités.php
@@ -1,32 +1,33 @@
+<!--AFFICHE LES ACTUALITÉS-->
 <?php
   /* Template Name: Actualités */
 
-$args = array(
-      'port_type' => 'post',
-);
+  $args = array(
+        'port_type' => 'post',
+  );
 
-$query= new WP_Query($args);
+  $query= new WP_Query($args);
 
-get_header(); ?>
+  get_header();
+?>
 
   <!--The Loop-->
-  <?php if ( $query ->have_posts() ) : while ( $query ->have_posts() ) : $query ->the_post(); ?>
+<?php if ( $query ->have_posts() ) : while ( $query ->have_posts() ) : $query ->the_post(); ?>
 
-    <section>
-      <h3>
-        <a href="<?php the_permalink(); ?>">
-          <?php the_title(); ?> Titre du swag </h3>
-        </a>
-      </section>
+  <section>
+    <h3>
+      <a href="<?php the_permalink(); ?>">
+        <?php the_title(); ?> Titre du swag </h3>
+      </a>
+    </section>
 
-      <p> <?php the_content(); ?>
-        <img src="screenshot.png" alt="Photo poney" title="Photo de poney"/>
-      </p>
+    <p> <?php the_content(); ?>
+      <img src="screenshot.png" alt="Photo poney" title="Photo de poney"/>
+    </p>
 
-  <?php endwhile; endif; ?>
+<?php endwhile; endif; ?>
 
   <p>Publié le : <?php the_date('j F, Y'); ?> à <?php the_time('G:i a'); ?></p>
 
-  <?php   get_footer();
-  /**include ('footer.php');*/
-   ?>
+  <!--AFFICHE LE FOOTER-->
+  <?php   get_footer(); ?>
